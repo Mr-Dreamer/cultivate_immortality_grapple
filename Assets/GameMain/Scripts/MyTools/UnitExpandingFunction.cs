e1756b83f12961f4b08582c19658ea843601de18
zhaozhiwei <sandman828@163.com>
2023-04-24 18:29:03 +0800

    日常

---
+++ Assets/GameMain/Scripts/MyTools/UnitExpandingFunction.cs
@@ -0,0 +1,24 @@
+// ---------------------------------------------------------------
+// 文件名称：UnitExpandingFunction.cs
+// 创 建 者：
+// 创建时间：2023/04/24
+// 功能描述：
+// ---------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitExpandingFunction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
