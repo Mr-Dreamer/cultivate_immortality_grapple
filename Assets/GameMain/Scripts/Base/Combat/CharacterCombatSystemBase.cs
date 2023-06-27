329b0622f6abb6a8061cc532ceeb157bf3d179d3
Mr-Dreamer <471222361@qq.com>
2023-06-27 08:38:33 +0800

    新增攻击基类；玩家攻击类；伤害接口；

---
+++ Assets/GameMain/Scripts/Base/Combat/CharacterCombatSystemBase.cs
@@ -0,0 +1,77 @@
+// ---------------------------------------------------------------
+// 文件名称：CharacterCombatSystemBase.cs
+// 创 建 者：
+// 创建时间：2023/06/26
+// 功能描述：攻击基类
+// ---------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using Grapple.Move;
+using UnityEngine;
+
+namespace Grapple.Combat
+{
+    public class CharacterCombatSystemBase : MonoBehaviour
+    {
+        protected Animator m_Animator;
+        protected CharacterInputSystem m_CharacterInputSystem;
+        protected CharacterMovementBase m_CharacterMovementBase;
+        protected AudioSource m_AudioSource;
+
+
+        //动画状态机中变量参数的哈希值
+        protected int m_LAtkID = Animator.StringToHash("LAtk");
+        protected int m_RAtkID = Animator.StringToHash("RAtk");
+        protected int m_DefenID = Animator.StringToHash("Defen");
+        protected int m_AnimationMoveID = Animator.StringToHash("AnimationMove");
+
+        //攻击检测
+        [SerializeField, Header("攻击检测")] protected Transform m_AttackDetectionCenter;
+        [SerializeField] protected float m_AttackDetectionRang;
+        [SerializeField] protected LayerMask m_EnemyLayer;
+
+        protected virtual void Awake()
+        {
+            m_Animator = GetComponent<Animator>();
+            m_CharacterInputSystem = GetComponentInParent<CharacterInputSystem>();
+            m_CharacterMovementBase = GetComponentInParent<CharacterMovementBase>();
+            m_AudioSource = m_CharacterMovementBase.GetComponentInChildren<AudioSource>();
+        }
+
+
+
+
+
+        /// <summary>
+        /// 攻击动画攻击检测事件
+        /// </summary>
+        /// <param name="hitName">传递受伤动画名</param>
+        protected virtual void OnAnimationAttackEvent(string hitName)
+        {
+            if (!m_Animator.CheckAnimationTag("Attack")) return;
+
+            Collider[] attackDetectionTargets = new Collider[4];
+
+            int counts = Physics.OverlapSphereNonAlloc(m_AttackDetectionCenter.position, m_AttackDetectionRang,
+                attackDetectionTargets, m_EnemyLayer);
+
+            if (counts > 0)
+            {
+                for (int i = 0; i < counts; i++)
+                {
+                    if (attackDetectionTargets[i].TryGetComponent(out IDamagar damagar))
+                    {
+                        damagar.TakeDamager(hitName);
+
+                    }
+                }
+            }
+            //TODO音效
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(m_AttackDetectionCenter.position, m_AttackDetectionRang);
+        }
+    }
+}
---
+++ Assets/GameMain/Scripts/Interface/IDamagar.cs
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------
+// 文件名称：IDamagar.cs
+// 创 建 者：
+// 创建时间：2023/06/26
+// 功能描述：攻击伤害接口
+// ---------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagar
+{
+    void TakeDamager(float damager);
+    void TakeDamager(string hitAnimationName);
+    void TakeDamager(float damager, string hitAnimationName);
+    void TakeDamager(float damagar, string hitAnimationName, Transform attacker);
+}
